9db1e0014ed976f22255c2635e409fd6e3a89e2b
279437541 <78850005+279437541@users.noreply.github.com>
2021-10-15 20:50:20 +0800

    Add files via upload

--- docker.sh
+++ docker.sh
@@ -24,86 +24,143 @@ TIME() {
 	echo
 	exit 1
 }
-if [[ `lsb_release -a | grep -c "buntu"` -ge '1' ]]; then
-	export Ubuntu="ubuntu"
-elif [[ `lsb_release -a | grep -c "ebian"` -ge '1' ]]; then
-	export Debian="debian"
-fi
-if [[ -z "${Ubuntu}" ]] && [[ -z "${Debian}" ]]; then
+
+if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
+	export Aptget="yum"
+	export XITONG="cent_os"
+elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
+	export Aptget="apt-get"
+	export XITONG="ubuntu_os"
+elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
+	export Aptget="apt"
+	export XITONG="debian_os"
+else
 	echo
-	TIME r "本脚本只适用于Ubuntu和Debian安装docker"
+	TIME y "本一键安装docker脚本只支持（centos、ubuntu和debian）!"
 	echo
 	exit 1
 fi
-apt install -y sudo
-apt install -y dpkg
-if [[ `dpkg -l | grep -c "docker"` -ge '1' ]]; then
+
+if [[ `docker --version | grep -c "version"` -ge '1' ]]; then
 	echo
 	TIME y "检测到docker存在，是否重新安装?"
 	echo
-	TIME g "重新安装会把您现有的所以容器及镜像全部删除"
+	TIME g "重新安装会把您现有的所有容器及镜像全部删除，请慎重!"
 	echo
+	while :; do
 	read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " ANDK
 	case $ANDK in
 		[Yy])
 			TIME g "正在御载老版本docker"
+			export CHONGXIN="YES"
 			docker stop $(docker ps -a -q)
 			docker rm $(docker ps -a -q)
 			docker rmi $(docker images -q)
-			sudo -E apt-get -qq remove -y docker docker-engine docker.io containerd runc
-			sudo -E apt-get -qq remove -y docker
-			sudo -E apt-get -qq remove -y docker-ce
-			sudo -E apt-get -qq remove -y docker-ce-cli
-			sudo -E apt-get -qq remove -y docker-ce-rootless-extras
-			sudo -E apt-get -qq remove -y docker-scan-plugin
-			sudo -E apt-get -qq remove -y --auto-remove docker
+			sudo "${Aptget}" remove -y docker docker-engine docker.io containerd runc
+			sudo "${Aptget}" remove -y docker
+			sudo "${Aptget}" remove -y docker-ce
+			sudo "${Aptget}" remove -y docker-ce-cli
+			sudo "${Aptget}" remove -y docker-ce-rootless-extras
+			sudo "${Aptget}" remove -y docker-scan-plugin
+			sudo "${Aptget}" remove -y --auto-remove docker
 			sudo rm -rf /var/lib/docker
 			sudo rm -rf /etc/docker
 			sudo rm -rf /lib/systemd/system/{docker.service,docker.socket}
 			rm /var/lib/dpkg/info/$nomdupaquet* -f
+		break
 		;;
 		[Nn])
-			TIME r "退出安装程序!"
-			sleep 2
+			echo
+			TIME r "您选择了退出程序!"
+			echo
+			sleep 3
 			exit 1
+		break
+    		;;
+    		*)
+			echo
+			TIME b "提示：请输入正确的选择!"
+			echo
 		;;
 	esac
+	done
 fi
-sudo -E apt-get -qq update
-sudo -E apt-get -qq install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-if [[ "${Ubuntu}" == "ubuntu" ]]; then
+echo
+TIME y "正在安装docker，请耐心等候..."
+"${Aptget}" -y update
+"${Aptget}" install -y sudo curl
+echo
+if [[ ${XITONG} == "cent_os" ]]; then
+	sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+	sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+	sudo yum -y update
+	sudo yum install -y docker-ce
+	sudo yum install -y docker-ce-cli
+	sudo yum install -y containerd.io
+	sudo yum install -y docker.io
+fi
+if [[ ${XITONG} == "ubuntu_os" ]]; then
+	sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-else
-	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
-fi
-sudo apt-key fingerprint 0EBFCD88
-if [[ "${Ubuntu}" == "ubuntu" ]]; then
+	if [[ $? -ne 0 ]];then
+		curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+	fi
+	sudo apt-key fingerprint 0EBFCD88
+	if [[ `sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88"` = '0' ]]; then
+		TIME r "密匙验证出错，或者没下载到密匙了，请检查网络，或者源有问题"
+		sleep 5
+		exit 1
+		sleep 5
+	fi
 	sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
-else
-	sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install -y docker-ce
+	sudo apt-get install -y docker-ce-cli
+	sudo apt-get install -y containerd.io
+	sudo apt-get install -y docker.io
 fi
-sudo -E apt-get -qq update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io |tee build.log
-if [[ `grep -c "dockerd -H fd://" build.log` -ge '1' ]]; then
-	TIME g "检测到文件有错误，尝试修复"
+if [[ ${XITONG} == "debian_os" ]]; then
+	sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+	if [[ $? -ne 0 ]];then
+		curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+	fi
+	sudo apt-key fingerprint 0EBFCD88
+	if [[ `sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88"` = '0' ]]; then
+		TIME r "密匙验证出错，或者没下载到密匙了，请检查网络，或者上游有问题"
+		sleep 5
+		exit 1
+		sleep 5
+	fi
+	sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install -y docker-ce
+	sudo apt-get install -y docker-ce-cli
+	sudo apt-get install -y containerd.io
+	sudo apt-get install -y docker.io
+fi
+
+if [[ "${CHONGXIN}" == "YES" ]]; then
 	sudo rm -fr /etc/systemd/system/docker.service.d
 	sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd#g' /lib/systemd/system/docker.service
 	sudo systemctl daemon-reload
 fi
-sudo systemctl restart docker
-sudo rm -fr build.log
 sudo rm -fr docker.sh
-sleep 10
-if [[ `dpkg -l | grep -c "docker"` -eq '0' ]]; then
+if [[ `docker --version | grep -c "version"` = '0' ]]; then
 	TIME y "docker安装失败"
 	sleep 2
 	exit 1
 else
 	TIME y ""
+	TIME g "docker安装成功，正在启动docker，请稍后..."
+	sudo systemctl restart docker
+	sudo systemctl start docker
+	sleep 12
+	TIME y ""
 	TIME g "测试docker拉取镜像是否成功"
 	TIME y ""
-	sudo docker run hello-world
-	if [[ `docker ps -a | grep -c "hello-world"` -ge '1' ]]; then
+	sudo docker run hello-world |tee build.log
+	if [[ `docker ps -a | grep -c "hello-world"` -ge '1' ]] && [[ `grep -c "docs.docker" build.log` -ge '1' ]]; then
 		echo
 		TIME g "测试镜像拉取成功，正在删除测试镜像..."
 		echo
@@ -115,10 +172,13 @@ else
 		echo
 	else
 		echo
-		TIME y "docker安装成功虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的"
+		TIME y "docker虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的，或者容器网络问题"
 		echo
 		TIME y "重启服务器后，用 sudo docker run hello-world 命令测试吧，能拉取成功就成了"
 		echo
+		sleep 2
+		exit 1
 	fi
 fi
+rm -fr build.log
 exit 0
--- feverrun.sh
+++ feverrun.sh
@@ -1,3 +1,4 @@
+   
 #!/usr/bin/env bash
 # 
 ## 本脚本搬运并模仿 liuqitoday
@@ -23,12 +24,15 @@ TIME() {
 
 dir_shell=/ql/config
 dir_script=/ql/scripts
+dir_raw=/ql/raw
 config_shell_path=$dir_shell/config.sh
 extra_shell_path=$dir_shell/extra.sh
 code_shell_path=$dir_shell/code.sh
-disable_shell_path=$dir_script/disableDuplicateTasksImplement.py
+Disable_shell_path=$dir_script/Disable.py
 wskey_shell_path=$dir_script/wskey.py
-task_before_shell_path=$dir_shell/task_before.sh
+crypto_shell_path=$dir_raw/crypto-js.js
+wx_jysz_shell_path=$dir_raw/wx_jysz.js
+OpenCard_shell_path=$dir_script/raw_jd_OpenCard.py
 sample_shell_path=/ql/sample/config.sample.sh
 
 rm -rf feverrun.sh
@@ -37,8 +41,8 @@ rm -rf feverrun.sh
 if [ ! -a "$config_shell_path" ]; then
     touch $config_shell_path
 fi
-curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun/config.sample.sh > $sample_shell_path
-curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/feverrun/config.sample.sh > $sample_shell_path
+curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/config.sample.sh > $sample_shell_path
+curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/config.sample.sh > $sample_shell_path
 cp $sample_shell_path $config_shell_path
 
 # 判断是否下载成功
@@ -54,8 +58,8 @@ fi
 if [ ! -a "$wskey_shell_path" ]; then
     touch $wskey_shell_path
 fi
-curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun/wskey.py > $wskey_shell_path
-curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/feverrun/wskey.py > $wskey_shell_path
+curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/wskey.py > $wskey_shell_path
+curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/wskey.py > $wskey_shell_path
 cp $wskey_shell_path $dir_script/wskey.py
 
 # 判断是否下载成功
@@ -66,12 +70,28 @@ if (( $(echo "${wskey_size} < 100" | bc -l) )); then
     exit 0
 fi
 
+# 下载 raw_jd_OpenCard.py
+if [ ! -a "$OpenCard_shell_path" ]; then
+    touch $OpenCard_shell_path
+fi
+curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/raw_jd_OpenCard.py > $OpenCard_shell_path
+curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/raw_jd_OpenCard.py > $OpenCard_shell_path
+cp $OpenCard_shell_path $dir_script/raw_jd_OpenCard.py
+
+# 判断是否下载成功
+OpenCard_size=$(ls -l $OpenCard_shell_path | awk '{print $5}')
+if (( $(echo "${OpenCard_size} < 100" | bc -l) )); then
+    echo
+    TIME y "raw_jd_OpenCard.py 下载失败"
+    exit 0
+fi
+
 # 下载 extra.sh
 if [ ! -a "$extra_shell_path" ]; then
     touch $extra_shell_path
 fi
-curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun/extra.sh > $extra_shell_path
-curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/feverrun/extra.sh > $extra_shell_path
+curl -fsSL https://cdn.jsdelivr.net/gh/shidahuilang/QL-@main/feverrun/extra.sh > $extra_shell_path
+curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/feverrun/extra.sh > $extra_shell_path
 cp $extra_shell_path $dir_shell/extra.sh
 
 # 判断是否下载成功
@@ -82,6 +102,8 @@ if (( $(echo "${extra_size} < 100" | bc -l) )); then
     exit 0
 fi
 
+
+
 # 授权
 chmod -R +x $dir_shell
 
@@ -89,7 +111,7 @@ chmod -R +x $dir_shell
 if [ "$(grep -c extra /ql/config/crontab.list)" = 0 ]; then
     echo
     echo
-    TIME g "开始添加任务 ql extra"
+    TIME g "开始添加 [每8小时更新任务]"
     echo
     echo
     # 获取token
@@ -100,19 +122,19 @@ fi
 if [ "$(grep -c wskey.py /ql/config/crontab.list)" = 0 ]; then
     echo
     echo
-    TIME g "开始添加任务 task wskey.py"
+    TIME g "开始添加 [每天检测WSKEY]"
     echo
     echo
     # 获取token
     token=$(cat /ql/config/auth.json | jq --raw-output .token)
-    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"每天检测WSKEY","command":"task wskey.py","schedule":"15 1 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1633428022377'
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"每天检测WSKEY","command":"task wskey.py","schedule":"15 22 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1633428022377'
 fi
 
 # 将 bot 添加到定时任务
 if [ "$(grep -c bot /ql/config/crontab.list)" = 0 ]; then
     echo
     echo
-    echo "开始添加 ql bot"
+    TIME g "开始添加 [拉取机器人]"
     echo
     echo
     # 获取token
@@ -120,25 +142,37 @@ if [ "$(grep -c bot /ql/config/crontab.list)" = 0 ]; then
     curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"拉取机器人","command":"ql bot","schedule":"13 14 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1626247933219'
 fi
 
+# 将 raw_jd_OpenCard.py 添加到定时任务
+if [ "$(grep -c raw_jd_OpenCard.py /ql/config/crontab.list)" = 0 ]; then
+    echo
+    echo
+    TIME g "开始添加 [JD入会开卡领取京豆]"
+    echo
+    echo
+    # 获取token
+    token=$(cat /ql/config/auth.json | jq --raw-output .token)
+    curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"JD入会开卡领取京豆","command":"task raw_jd_OpenCard.py","schedule":"8 8,15,20 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1634041221437'
+fi
+
 pip3 install requests
 
 if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 1 ]]; then
     echo
     TIME g "执行WSKEY转换PT_KEY操作"
-    task wskey.py
+    task wskey.py |tee azcg.log
     echo
-    if [[ "$(grep -c JD_COOKIE=\"pt_key= /ql/config/env.sh)" = 1 ]]; then
+    if [[ `ls -a |grep -c "wskey添加成功" /ql/azcg.log` -ge '1' ]] && [[ `ls -a |grep -c "wskey添加失败" /ql/azcg.log` = '0' ]]; then
     	echo
     	TIME g "WSKEY转换PT_KEY成功"
 	echo
     else
     	echo
-    	TIME g "WSKEY转换PT_KEY失败，检查KEY的格式对不对，或者有没有失效，然后在任务里手动转换"
+    	TIME g "WSKEY转换PT_KEY失败，检查KEY的格式对不对，或者有没有失效，如果是多个WSKEY的话，或者有个别KEY出问题"
 	echo
     fi
 fi
-
-ql extra
+rm -fr /ql/azcg.log
+ql extra |tee azcg.log
 
 echo
 if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 0 ]] && [[ "$(grep -c JD_COOKIE=\"pt_key= /ql/config/env.sh)" = 0 ]]; then
@@ -146,5 +180,62 @@ if [[ "$(grep -c JD_WSCK=\"pin= /ql/config/env.sh)" = 0 ]] && [[ "$(grep -c JD_C
 fi
 echo
 echo
-
+if [[ `ls -a |grep -c "添加成功" /ql/azcg.log` -ge '1' ]] && [[ `ls -a |grep -c "执行结束" /ql/azcg.log` -ge '1' ]]; then
+	TIME g "脚本安装完成，下面开始安装依赖!"
+	rm -fr /ql/azcg.log
+else
+	TIME r "脚本安装失败，请用一键单独安装任务重新尝试!"
+	rm -fr /ql/azcg.log
+	exit 1
+fi
+echo
+echo
+echo
+echo
+TIME l "安装依赖，依赖必须安装，要不然脚本不运行"
+echo
+TIME y "建议使用翻墙网络安装，要不然安装依赖的时候你会急死的"
+echo
+TIME g "没翻墙条件，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
+echo
+TIME y "依赖安装时看到显示ERR!错误提示不用管，只要依赖能从头到尾的下载运行完毕就好了"
+echo
+TIME g "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了，到时候可以使用我的一键独立安装依赖脚本来安装"
+echo
+sleep 5
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
+cd /ql
+npm install -g typescript
+cd /ql
+npm install axios date-fns
+cd /ql
+npm install crypto -g
+cd /ql
+npm install jsdom
+cd /ql
+npm install png-js
+cd /ql
+npm install -g npm
+cd /ql
+pnpm i png-js
+cd /ql
+pip3 install requests
+cd /ql
+apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
+cd /ql
+apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+cd /ql
+package_name="canvas png-js date-fns axios crypto-js ts-md5 tslib @types/node dotenv typescript fs require tslib"
+for i in $package_name; do
+    case $i in
+        canvas)
+            cd /ql/scripts
+            npm ls $i
+            ;;
+        *)
+            npm ls $i -g
+            ;;
+    esac
+done
+TIME g "所有依赖安装完毕"
 exit 0
