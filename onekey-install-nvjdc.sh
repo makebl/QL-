0b225ddd94155fc42b0f79350bcbd4db5b144f0d
279437541 <78850005+279437541@users.noreply.github.com>
2021-11-21 13:19:31 +0800

    Update onekey-install-nvjdc.sh

--- onekey-install-nvjdc.sh
+++ onekey-install-nvjdc.sh
@@ -82,13 +82,12 @@ docker pull 10529459/lanyannvjdc:1.4
 apt install wget unzip -y
 cd /root/nvjdc
 mkdir -p  Config && cd Config
-cd .. && cd ..
 read -p "请输入青龙服务器在web页面中显示的名称: " QLName && printf "\n"
 read -p "请输入青龙OpenApi Client ID: " ClientID && printf "\n"
 read -p "请输入青龙OpenApi Client Secret: " ClientSecret && printf "\n"
 read -p "请输入青龙服务器的url地址（类似http://192.168.2.2:5700）: " QLurl && printf "\n"
 read -p "请输入nvjdc面板希望使用的端口号: " jdcport && printf "\n"
-cat >> Config.json << EOF
+cat >> Config/Config.json << EOF
 {
     ///最大支持几个网页
     "MaxTab": "20",
