6f2d293088c922adb80d2a66c5fac2d4519e9558
279437541 <78850005+279437541@users.noreply.github.com>
2021-11-26 16:08:54 +0800

    Update nv.sh

--- nv.sh
+++ nv.sh
@@ -161,9 +161,12 @@ exit
 install_nvjdc(){
 echo -e "${red}开始进行安装,请根据命令提示操作${plain}"
 git clone https://github.com/btlanyan/nvjdc.git /root/nvjdc
-mkdir nvjdc && cd nvjdc 
+mkdir nvjdc && cd nvjdc
+echo
 mkdir -p  .local-chromium/Linux-884014 && cd .local-chromium/Linux-884014
-wget https://mirrors.huaweicloud.com/chromium-browser-snapshots/Linux_x64/884014/chrome-linux.zip > /dev/null 2>&1 
+echo
+wget https://mirrors.huaweicloud.com/chromium-browser-snapshots/Linux_x64/884014/chrome-linux.zip > /dev/null 2>&1
+echo
 unzip chrome-linux.zip > /dev/null 2>&1 
 rm  -f chrome-linux.zip > /dev/null 2>&1 
 
