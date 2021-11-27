b4ddac90b15e991a91ac1fea2b07315cfdea25b6
279437541 <78850005+279437541@users.noreply.github.com>
2021-11-27 22:05:33 +0800

    Update nv.sh

--- nv.sh
+++ nv.sh
@@ -168,7 +168,7 @@ wget https://mirrors.huaweicloud.com/chromium-browser-snapshots/Linux_x64/884014
 #unzip chrome-linux.zip > /dev/null 2>&1 
 rm  -f chrome-linux.zip > /dev/null 2>&1 
 rm  -f /root/nvjdc/Config/Config.json
-cd .. && cd ..
+cd /root/nvjdc/Config && cd /root/nvjdc/Config
 read -p "请输入青龙服务器在web页面中显示的名称: " QLName && printf "\n"
 read -p "请输入青龙OpenApi Client ID: " ClientID && printf "\n"
 read -p "请输入青龙OpenApi Client Secret: " ClientSecret && printf "\n"
