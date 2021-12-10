de60ef7db8ad2020841a847b553a1af77d9f84ca
279437541 <78850005+279437541@users.noreply.github.com>
2021-12-10 15:44:41 +0800

    Update dong.sh

--- dong.sh
+++ dong.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-stty erase ^H
-PORT=0
-#判断当前端口是否被占用，没被占用返回0，反之1
+
 function Listening {
    TCPListeningnum=`netstat -an | grep ":$1 " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l`
    UDPListeningnum=`netstat -an | grep ":$1 " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l`
