019d31375680f7ae2b6d3f9257123e4acb98b4d9
279437541 <78850005+279437541@users.noreply.github.com>
2021-12-10 15:57:04 +0800

    Update dong.sh

--- dong.sh
+++ dong.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
 
-    r) export Color="\e[31;1m" ;;
-    g) export Color="\e[32;1m" ;;
-    b) export Color="\e[34;1m" ;;
-    y) export Color="\e[33;1m" ;;
-    z) export Color="\e[35;1m" ;;
-    l) export Color="\e[36;1m" ;;
-    esac
-    [[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
-      echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
-
+TIME() {
+[[ -z "$1" ]] && {
+	echo -ne " "
+} || {
+     case $1 in
+	r) export Color="\e[31;1m";;
+	g) export Color="\e[32;1m";;
+	b) export Color="\e[34;1m";;
+	y) export Color="\e[33;1m";;
+	z) export Color="\e[35;1m";;
+	l) export Color="\e[36;1m";;
+      esac
+	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+	 }
+      }
+}
 
 [[ ! "$USER" == "root" ]] && {
   echo
