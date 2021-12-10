a1c624ce876cbee07ffc4cb1c2d02e232de40c35
279437541 <78850005+279437541@users.noreply.github.com>
2021-12-10 15:46:12 +0800

    Update dong.sh

--- dong.sh
+++ dong.sh
@@ -1,32 +1,5 @@
 #!/usr/bin/env bash
 
-function Listening {
-   TCPListeningnum=`netstat -an | grep ":$1 " | awk '$1 == "tcp" && $NF == "LISTEN" {print $0}' | wc -l`
-   UDPListeningnum=`netstat -an | grep ":$1 " | awk '$1 == "udp" && $NF == "0.0.0.0:*" {print $0}' | wc -l`
-   (( Listeningnum = TCPListeningnum + UDPListeningnum ))
-   if [ $Listeningnum == 0 ]; then
-       echo "0"
-   else
-       echo "1"
-
-}
-
-#指定区间随机数
-function random_range {
-   shuf -i $1-$2 -n1
-}
-
-#得到随机端口
-function get_random_port {
-   templ=0
-   while [ $PORT == 0 ]; do
-       temp1=`random_range $1 $2`
-       if [ `Listening $temp1` == 0 ] ; then
-              PORT=$temp1
-       fi
-   done
-   echo "port=$PORT"
-}
 
 TIME() {
   [[ -z "$1" ]] && {
