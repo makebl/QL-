5f4f0d586cb44ba671a1339ae9da13021c17f063
279437541 <78850005+279437541@users.noreply.github.com>
2021-12-10 16:02:46 +0800

    Update dong.sh

--- dong.sh
+++ dong.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# 
-#!/usr/bin/env bash
 stty erase ^H
 PORT=0
 #判断当前端口是否被占用，没被占用返回0，反之1
@@ -31,22 +29,23 @@ function get_random_port {
    done
    echo "port=$PORT"
 }
+
 TIME() {
-[[ -z "$1" ]] && {
-	echo -ne " "
-} || {
-     case $1 in
-	r) export Color="\e[31;1m";;
-	g) export Color="\e[32;1m";;
-	b) export Color="\e[34;1m";;
-	y) export Color="\e[33;1m";;
-	z) export Color="\e[35;1m";;
-	l) export Color="\e[36;1m";;
-      esac
-	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
-		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
-	 }
-      }
+  [[ -z "$1" ]] && {
+    echo -ne " "
+  } || {
+    case $1 in
+    r) export Color="\e[31;1m" ;;
+    g) export Color="\e[32;1m" ;;
+    b) export Color="\e[34;1m" ;;
+    y) export Color="\e[33;1m" ;;
+    z) export Color="\e[35;1m" ;;
+    l) export Color="\e[36;1m" ;;
+    esac
+    [[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+      echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+    }
+  }
 }
 
 [[ ! "$USER" == "root" ]] && {
@@ -73,7 +72,160 @@ elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
   export XITONG="debian_os"
 else
   echo
+  TIME y "本一键安装docker脚本只支持（centos、ubuntu和debian）!"
+  echo
+  exit 1
+fi
 
+ignore_install_docker=0
+if [[ $(docker --version | grep -c "version") -ge '1' ]]; then
+  echo
+  TIME y "检测到docker存在，是否重新安装?"
+  echo
+  TIME g "重新安装会把您现有的所有容器及镜像全部删除，请慎重!"
+  echo
+  while :; do
+    read -p " [输入[ N/n ]回车跳过安装docker，输入[ Y/y ]回车重新安装docker]： " ANDK
+    case $ANDK in
+    [Yy])
+      TIME g "正在御载老版本docker"
+      export CHONGXIN="YES"
+      docker stop $(docker ps -a -q)
+      docker rm $(docker ps -a -q)
+      docker rmi $(docker images -q)
+      sudo "${Aptget}" remove -y docker docker-engine docker.io containerd runc
+      sudo "${Aptget}" remove -y docker
+      sudo "${Aptget}" remove -y docker-ce
+      sudo "${Aptget}" remove -y docker-ce-cli
+      sudo "${Aptget}" remove -y docker-ce-rootless-extras
+      sudo "${Aptget}" remove -y docker-scan-plugin
+      sudo "${Aptget}" remove -y --auto-remove docker
+      sudo rm -rf /var/lib/docker
+      sudo rm -rf /etc/docker
+      sudo rm -rf /lib/systemd/system/{docker.service,docker.socket}
+      rm /var/lib/dpkg/info/$nomdupaquet* -f
+      break
+      ;;
+    [Nn])
+      echo
+      TIME r "跳过安装docker!"
+      echo
+      sleep 1
+      ignore_install_docker=1
+      break
+      ;;
+    *)
+      echo
+      TIME b "提示：请输入正确的选择!"
+      echo
+      ;;
+    esac
+  done
+fi
+
+if [ $ignore_install_docker == 0 ]; then
+  echo
+  TIME y "正在安装docker，请耐心等候..."
+  echo
+  if [[ ${XITONG} == "cent_os" ]]; then
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+    sudo yum -y update
+    sudo yum install -y docker-ce
+    sudo yum install -y docker-ce-cli
+    sudo yum install -y containerd.io
+  fi
+  if [[ ${XITONG} == "ubuntu_os" ]]; then
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+    if [[ $? -ne 0 ]]; then
+      curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+    fi
+    sudo apt-key fingerprint 0EBFCD88
+    if [[ $(sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88") == '0' ]]; then
+      TIME r "密匙验证出错，或者没下载到密匙了，请检查网络，或者源有问题"
+      sleep 5
+      exit 1
+      sleep 5
+    fi
+    sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    sudo apt-get install -y docker-ce-cli
+    sudo apt-get install -y containerd.io
+  fi
+  if [[ ${XITONG} == "debian_os" ]]; then
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+    if [[ $? -ne 0 ]]; then
+      curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+    fi
+    sudo apt-key fingerprint 0EBFCD88
+    if [[ $(sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88") == '0' ]]; then
+      TIME r "密匙验证出错，或者没下载到密匙了，请检查网络，或者上游有问题"
+      sleep 5
+      exit 1
+      sleep 5
+    fi
+    sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    sudo apt-get install -y docker-ce-cli
+    sudo apt-get install -y containerd.io
+  fi
+  sudo rm -fr /etc/systemd/system/docker.service.d
+  sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd#g' /lib/systemd/system/docker.service
+  sudo systemctl daemon-reload
+  sudo rm -fr docker.sh
+  if [[ $(docker --version | grep -c "version") == '0' ]]; then
+    TIME y "docker安装失败"
+    sleep 2
+    exit 1
+  else
+    # 1.创建一个目录
+    sudo mkdir -p /etc/docker
+
+    # 2.编写配置文件
+    sudo tee /etc/docker/daemon.json <<-'EOF'
+      {
+        "registry-mirrors": ["http://hub-mirror.c.163.com",
+          "https://docker.mirrors.ustc.edu.cn",
+          "https://mirror.ccs.tencentyun.com"
+        ]
+      }
+EOF
+
+    TIME y ""
+    TIME g "docker安装成功，正在启动docker，请稍后..."
+    sudo systemctl restart docker
+    sudo systemctl start docker
+    sudo systemctl enable docker.service
+    sleep 12
+    TIME y ""
+    TIME g "测试docker拉取镜像是否成功"
+    TIME y ""
+    sudo docker run hello-world | tee build.log
+    if [[ $(docker ps -a | grep -c "hello-world") -ge '1' ]] && [[ $(grep -c "docs.docker" build.log) -ge '1' ]]; then
+      echo
+      TIME g "测试镜像拉取成功，正在删除测试镜像..."
+      echo
+      docker stop $(docker ps -a -q)
+      docker rm $(docker ps -a -q)
+      docker rmi $(docker images -q)
+      echo
+      TIME y "测试镜像删除完毕，docker安装成功!"
+      echo
+    else
+      echo
+      TIME y "docker虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的，或者容器网络问题"
+      echo
+      TIME y "重启服务器后，用 sudo docker run hello-world 命令测试吧，能拉取成功就成了"
+      echo
+      sleep 2
+      exit 1
+    fi
+  fi
+  rm -fr build.log
 fi
 
 dir='jd-qinglong'
@@ -103,16 +255,16 @@ docker rm -f webapp
 docker pull rubyangxg/jd-qinglong
 
 ad_port1=5701
-echo "请设置阿东网页登录端口：(数字5701~65535)，回车默认5703"
+echo "请设置阿东网页登录端口：(数字5701~65535)，回车默认5701"
 while [ 1 ]; do
   read input
   if [ -z "${input}" ]; then
-    input=5703
+    input=5701
   fi
   if [ $input -gt 5700 -a $input -lt 65536 ]; then
     grep_port=$(netstat -tlpn | grep "\b$input\b")
     if [ -n "$grep_port" ]; then
-      get_random_port 5703 5800
+      get_random_port 5701 5800
       ad_port1=$PORT
       echo -e "端口 $input 已被占用，生成随机端口$ad_port1，配置成功\n"
     else
@@ -121,21 +273,21 @@ while [ 1 ]; do
     fi
     break
   else
-    echo "别瞎搞，请输入端口：(数字5703~65535)"
+    echo "别瞎搞，请输入端口：(数字5701~65535)"
   fi
 done
 
-ad_port2=5704
-echo "请设置阿东网页管理(内部使用)端口：(数字5704~65535)，回车默认5702"
+ad_port2=5702
+echo "请设置阿东网页管理(内部使用)端口：(数字5702~65535)，回车默认5702"
 while [ 1 ]; do
   read input
   if [ -z "${input}" ]; then
-    input=5704
+    input=5702
   fi
-  if [ $input -gt 5703 -a $input -lt 65536 ]; then
+  if [ $input -gt 5701 -a $input -lt 65536 ]; then
     grep_port=$(netstat -tlpn | grep "\b$input\b")
     if [ -n "$grep_port" ]; then
-      get_random_port 5704 5800
+      get_random_port 5702 5800
       ad_port2=$PORT
       echo -e "端口 $input 已被占用，生成随机端口$ad_port2，配置成功\n"
     else
@@ -144,7 +296,7 @@ while [ 1 ]; do
     fi
     break
   else
-    echo "别瞎搞，请输入端口：(数字5704~65535)"
+    echo "别瞎搞，请输入端口：(数字5702~65535)"
   fi
 done
 
