7804adad9638f5e9623270fbd0bdf818ce0cc60b
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-22 21:50:44 +0800

    Update 4444.sh

--- 4444.sh
+++ 4444.sh
@@ -161,7 +161,7 @@ cd /root/Rabbit && mkdir -p  Config
 #cd /root/Rabbit/Config && wget -O Config.json  https://raw.githubusercontent.com/ht944/MadRabbit/main/Config.json
 cd /root/Rabbit
 
-mkdir /root/Rabbit/Config/Config.json && cd /root/Rabbit/Config/Config.json
+#mkdir /root/Rabbit/Config/Config.json && cd /root/Rabbit/Config/Config.json
 wget -O Config.json   https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/Config.json
 read -p "请输入青龙服务器在web页面中显示的名称: " QLName && printf "\n"
 read -p "请输入Rabbit面板标题: " Title && printf "\n"
