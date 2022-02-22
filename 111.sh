b2f5a4129db6d107d3fea93b6bc8efa77f7d9ec4
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-22 21:13:25 +0800

    Update 111.sh

--- 111.sh
+++ 111.sh
@@ -161,7 +161,6 @@ cd /root && mkdir -p  Rabbit && cd Rabbit
 cd /root/Rabbit && mkdir -p  Config
 cd /root/Rabbit/Config && wget -O Config.json  https://raw.githubusercontent.com/ht944/MadRabbit/main/Config.json
 cd /root/Rabbit
-fi
 mkdir /root/Rabbit/Config && cd /root/Rabbit/Config
 wget -O Config.json   https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/Config.json
 read -p "请输入青龙服务器在web页面中显示的名称: " QLName && printf "\n"
