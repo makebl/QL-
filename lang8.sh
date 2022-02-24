dd077055b3c70d4b638005cd469eb44f5e7de3c7
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-24 15:22:50 +0800

    Update and rename lang7.sh to lang8.sh

--- lang8.sh
+++ lang8.sh
@@ -175,11 +175,8 @@ function qinglong_port() {
   esac
 }
 
-#function system_check() {
+function system_check() {
 if [[ $synology == 1 ]]; then
-  echo
-  TIME y "你是群晖nas"
-  echo
 elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
     ECHOG "正在安装宿主机所需要的依赖，请稍后..."
     export QL_PATH="/opt"
