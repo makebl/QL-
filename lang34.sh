9ff087fa350f663fd0c9c4f85d7ae9a5c4055316
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-24 17:37:04 +0800

    Update and rename lang33.sh to lang34.sh

--- lang34.sh
+++ lang34.sh
@@ -119,8 +119,7 @@ elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" || "$(. /etc/os-release
     elif [[ -d /mnt/mmcblk2p4/docker ]]; then
       export QL_PATH="/root"
       export QL_Kongjian="/mnt/mmcblk2p4/docker"
-
-}
+fi
 
 
 export Current="$PWD"
