8fa556a22628a4b647d82fe87870cfa51b0ecd48
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-24 17:43:34 +0800

    Update and rename lang38.sh to lang39.sh

--- lang39.sh
+++ lang39.sh
@@ -85,13 +85,15 @@ if [[ ! "$USER" == "root" ]]; then
   exit 1
 fi
 
-function system_check() {
 if [[ $synology == 1 ]]; then
   echo
   TIME y "你是群晖nas"
   echo
 elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
-yum -y install sudo wget git unzip net-tools.x86_64 subversion
+  export Aptget="yum"
+  yum -y update
+  yum install -y sudo wget curl psmisc net-tools
+  export XITONG="cent_os"
 elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
   export Aptget="apt-get"
   apt-get -y update
@@ -102,10 +104,16 @@ elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" || "$(. /etc/os-release
   apt-get -y update
   apt-get install -y sudo wget curl psmisc net-tools
   export XITONG="debian_os"
-
-    exit 1
-  fi
-}
+elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
+  export Aptget="opkg"
+  opkg update
+  opkg install git-http > /dev/null 2>&1
+  opkg install ca-bundle > /dev/null 2>&1
+  opkg install coreutils-timeout > /dev/null 2>&1
+  opkg install findutils-xargs > /dev/null 2>&1
+  opkg install unzip
+else
+  echo
 
 
 export Current="$PWD"
