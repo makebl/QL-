89ae3a31e9f76fc1a5c2dfca6d1551f8275998cc
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-24 15:41:47 +0800

    Update and rename lang14.sh to lang15.sh

--- lang15.sh
+++ lang15.sh
@@ -189,25 +189,21 @@ if [[ $synology == 1 ]]; then
   echo
   TIME y "你是群晖nas"
   echo
-  elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
-    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
-    export QL_PATH="/opt"
-    yum -y install sudo wget git unzip net-tools.x86_64 subversion
-  elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
-    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
-    export QL_PATH="/opt"
-    apt-get -y update
-    apt-get -y install sudo wget git unzip net-tools subversion
-  elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
-    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
-    export QL_PATH="/opt"
-    apt -y update
-    apt -y install sudo wget git unzip net-tools subversion
-  elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
-    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
-    export QL_PATH="/opt"
-    apk update
-    apk add sudo wget git unzip net-tools subversion
+elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
+  export Aptget="yum"
+  yum -y update
+  yum install -y sudo wget curl psmisc net-tools
+  export XITONG="cent_os"
+elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
+  export Aptget="apt-get"
+  apt-get -y update
+  apt-get install -y sudo wget curl psmisc net-tools
+  export XITONG="ubuntu_os"
+elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" || "$(. /etc/os-release && echo "$ID")" == "Deepin" ]]; then
+  export Aptget="apt"
+  apt-get -y update
+  apt-get install -y sudo wget curl psmisc net-tools
+  export XITONG="debian_os"
   elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
     ECHOG "正在安装宿主机所需要的依赖，请稍后..."
     opkg update
