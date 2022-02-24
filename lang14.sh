a679590e2a58a8b49eb245f0d047f31c13756ca7
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-24 15:39:41 +0800

    Update and rename lang13.sh to lang14.sh

--- lang14.sh
+++ lang14.sh
@@ -18,11 +18,6 @@ RedBG="\033[41;37m"
 OK="${Green}[OK]${Font}"
 ERROR="${Red}[ERROR]${Font}"
 
-synology=0
-if [[ $(uname -a) == *synology* ]]; then
-  synology=1
-fi
-
 function print_ok() {
   echo
   echo -e " ${OK} ${Blue} $1 ${Font}"
@@ -74,6 +69,16 @@ judge() {
     exit 1
   fi
 }
+
+case $(uname -m) in
+x86_64) is_x86=1 ;;
+aarch64) is_x86=0 ;;
+esac
+
+synology=0
+if [[ $(uname -a) == *synology* ]]; then
+  synology=1
+fi
 
 if [[ ! "$USER" == "root" ]]; then
   print_error "警告：请使用root用户操作!~~"
@@ -180,213 +185,115 @@ function qinglong_port() {
   esac
 }
 
-function system_check() {
 if [[ $synology == 1 ]]; then
   echo
   TIME y "你是群晖nas"
   echo
-elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
-  export Aptget="yum"
-  yum -y update
-  yum install -y sudo wget curl psmisc net-tools
-  export XITONG="cent_os"
-elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
-  export Aptget="apt-get"
-  apt-get -y update
-  apt-get install -y sudo wget curl psmisc net-tools
-  export XITONG="ubuntu_os"
-elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" || "$(. /etc/os-release && echo "$ID")" == "Deepin" ]]; then
-  export Aptget="apt"
-  apt-get -y update
-  apt-get install -y sudo wget curl psmisc net-tools
-  export XITONG="debian_os"
-elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
-  export Aptget="opkg"
-  opkg update
-  opkg install git-http > /dev/null 2>&1
-  opkg install ca-bundle > /dev/null 2>&1
-  opkg install coreutils-timeout > /dev/null 2>&1
-  opkg install findutils-xargs > /dev/null 2>&1
-  opkg install unzip
-else
-  echo
-  TIME y "本一键安装docker脚本只支持（centos、ubuntu和debian,NAS,OpenWrt）!"
-  echo
-  exit 1
-fi
-
-ignore_install_docker=0
-if [[ $(docker --version | grep -c "version") -ge '1' ]]; then
-  echo
-  if [[ $synology == 1 ]]; then
-    TIME y "已安装docker，进行下一步"
-    ignore_install_docker=1
-  else
-    TIME y "检测到docker存在，是否重新安装?"
-    echo
-    TIME g "重新安装会把您现有的所有容器及镜像全部删除，请慎重!"
-    echo
-    while :; do
-      read -p " [输入[ N/n ]回车跳过安装docker，输入[ Y/y ]回车重新安装docker]： " ANDK
-      case $ANDK in
-      [Yy])
-        TIME g "正在御载老版本docker"
-        export CHONGXIN="YES"
-        docker stop $(docker ps -a -q)
-        docker rm $(docker ps -a -q)
-        docker rmi $(docker images -q)
-        sudo "${Aptget}" remove -y docker docker-engine docker.io containerd runc
-        sudo "${Aptget}" remove -y docker
-        sudo "${Aptget}" remove -y docker-ce
-        sudo "${Aptget}" remove -y docker-ce-cli
-        sudo "${Aptget}" remove -y docker-ce-rootless-extras
-        sudo "${Aptget}" remove -y docker-scan-plugin
-        sudo "${Aptget}" remove -y --auto-remove docker
-        sudo rm -rf /var/lib/docker
-        sudo rm -rf /etc/docker
-        sudo rm -rf /lib/systemd/system/{docker.service,docker.socket}
-        rm /var/lib/dpkg/info/$nomdupaquet* -f
-        break
-        ;;
-      [Nn])
-        echo
-        TIME r "跳过安装docker!"
-        echo
-        sleep 1
-        ignore_install_docker=1
-        break
-        ;;
-      *)
-        echo
-        TIME b "提示：请输入正确的选择!"
-        echo
-        ;;
-      esac
-    done
-  fi
-fi
-
-if [ $ignore_install_docker == 0 ]; then
-  echo
-  TIME y "正在安装docker，请耐心等候..."
-  echo
-  if [[ ${XITONG} == "cent_os" ]]; then
-    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-    sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-    sudo yum -y update
-    sudo yum install -y docker-ce
-    sudo yum install -y docker-ce-cli
-    sudo yum install -y containerd.io
-  fi
-  if [[ ${XITONG} == "ubuntu_os" ]]; then
-    if [ $is_x86 == 1 ]; then
-      sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-      curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-      if [[ $? -ne 0 ]]; then
-        curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-      fi
-      sudo apt-key fingerprint 0EBFCD88
-      if [[ $(sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88") == '0' ]]; then
-        TIME r "密匙验证出错，或者没下载到密匙了，请检查网络，或者源有问题"
-        sleep 5
-        exit 1
-        sleep 5
-      fi
-      sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
-      sudo apt-get update
-      sudo apt-get install -y docker-ce
-      sudo apt-get install -y docker-ce-cli
-      sudo apt-get install -y containerd.io
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    yum -y install sudo wget git unzip net-tools.x86_64 subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apt-get -y update
+    apt-get -y install sudo wget git unzip net-tools subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apt -y update
+    apt -y install sudo wget git unzip net-tools subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apk update
+    apk add sudo wget git unzip net-tools subversion
+  elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    opkg update
+    opkg install git-http > /dev/null 2>&1
+    opkg install ca-bundle > /dev/null 2>&1
+    opkg install coreutils-timeout > /dev/null 2>&1
+    opkg install findutils-xargs > /dev/null 2>&1
+    opkg install unzip
+    XTong="openwrt"
+    if [[ -d /opt/docker ]]; then
+      export QL_PATH="/opt"
+      export QL_Kongjian="/opt/docker"
+    elif [[ -d /mnt/mmcblk2p4/docker ]]; then
+      export QL_PATH="/root"
+      export QL_Kongjian="/mnt/mmcblk2p4/docker"
     else
-      sudo apt-get update
-      sudo apt-get install -y \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -f --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-      echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-      sudo apt-get update
-      sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-    fi
-  fi
-  if [[ ${XITONG} == "debian_os" ]]; then
-    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-    curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
-    if [[ $? -ne 0 ]]; then
-      curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
-    fi
-    sudo apt-key fingerprint 0EBFCD88
-    if [[ $(sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88") == '0' ]]; then
-      TIME r "密匙验证出错，或者没下载到密匙了，请检查网络，或者上游有问题"
-      sleep 5
+      print_error "没找到/opt/docker或者/mnt/mmcblk2p4/docker"
       exit 1
-      sleep 5
     fi
-    sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce
-    sudo apt-get install -y docker-ce-cli
-    sudo apt-get install -y containerd.io
-  fi
-  sudo rm -fr /etc/systemd/system/docker.service.d
-  sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd#g' /lib/systemd/system/docker.service
-  sudo systemctl daemon-reload
-  sudo rm -fr docker.sh
-  if [[ $(docker --version | grep -c "version") == '0' ]]; then
-    TIME y "docker安装失败"
-    sleep 2
+  else
+    print_error "不支持您的系统"
     exit 1
+  fi
+}
+
+function kaiqiroot_ssh() {
+  if [[ ! -f /etc/openwrt_release ]] && [[ ! -f /rom/etc/openwrt_release ]]; then
+    echo
+    ECHOGG "开启root用户ssh，方便使用工具连接服务器直接修改文件代码"
+    bash -c "$(curl -fsSL ${curlurl}/ssh.sh)"
+    judge "开启root用户ssh"
+    sleep 3
+  fi
+}
+
+function nolanjdc_lj() {
+  export Home="$QL_PATH/nolanjdc"
+  export Config="$Home/Config"
+  export Chromium="$Home/.local-chromium/Linux-884014"
+}
+
+function system_docker() {
+  if [[ "${XTong}" == "openwrt" ]]; then
+    if [[ ! -x "$(command -v docker)" ]]; then
+      print_error "没检测到docker，openwrt请自行安装docker，和挂载好硬盘"
+      sleep 1
+      exit 1
+    fi
   else
-    # 1.创建一个目录
-    sudo mkdir -p /etc/docker
+    if [[ ! -x "$(command -v docker)" ]]; then
+      ECHOR "没检测到docker，正在安装docker"
+      bash -c "$(curl -fsSL ${curlurl}/docker.sh)"
+    fi
+  fi
+}
 
-    # 2.编写配置文件
-    sudo tee /etc/docker/daemon.json <<-'EOF'
-      {
-        "registry-mirrors": ["http://hub-mirror.c.163.com",
-          "https://docker.mirrors.ustc.edu.cn",
-          "https://mirror.ccs.tencentyun.com"
-        ]
-      }
-EOF
-
-    TIME y ""
-    TIME g "docker安装成功，正在启动docker，请稍后..."
-    sudo systemctl restart docker
-    sudo systemctl start docker
-    sudo systemctl enable docker.service
-    sleep 12
-    TIME y ""
-    TIME g "测试docker拉取镜像是否成功"
-    TIME y ""
-    sudo docker run hello-world | tee build.log
-    if [[ $(docker ps -a | grep -c "hello-world") -ge '1' ]] && [[ $(grep -c "docs.docker" build.log) -ge '1' ]]; then
-      echo
-      TIME g "测试镜像拉取成功，正在删除测试镜像..."
-      echo
-      docker stop $(docker ps -a -q)
-      docker rm $(docker ps -a -q)
-      docker rmi $(docker images -q)
-      echo
-      TIME y "测试镜像删除完毕，docker安装成功!"
-      echo
+function systemctl_status() {
+  echo
+  if [[ "${XTong}" == "openwrt" ]]; then
+    /etc/init.d/dockerman start > /dev/null 2>&1
+    /etc/init.d/dockerd start > /dev/null 2>&1
+    sleep 3
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
+    service docker start > /dev/null 2>&1
+    sleep 1
+    if [[ `docker version |grep -c "runc"` == '1' ]]; then
+      print_ok "docker正在运行中!"
     else
-      echo
-      TIME y "docker虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的，或者容器网络问题"
-      echo
-      TIME y "重启服务器后，用 sudo docker run hello-world 命令测试吧，能拉取成功就成了"
-      echo
-      sleep 2
+      print_error "docker没有启动，请先启动docker，或者检查一下是否安装失败"
+      sleep 1
+      exit 1
+    fi
+  else
+    systemctl start docker > /dev/null 2>&1
+    sleep 1
+    echo
+    ECHOGG "检测docker是否在运行"
+    if [[ `systemctl status docker |grep -c "active (running) "` == '1' ]]; then
+      print_ok "docker正在运行中!"
+    else
+      print_error "docker没有启动，请先启动docker，或者检查一下是否安装失败"
+      sleep 1
       exit 1
     fi
   fi
-  rm -fr build.log
-fi
-
-
+}
 
 function uninstall_qinglong() {
   if [[ `docker images | grep -c "qinglong"` -ge '1' ]] || [[ `docker ps -a | grep -c "qinglong"` -ge '1' ]]; then
@@ -1158,5 +1065,5 @@ elif [[ `docker images | grep -c "qinglong"` -ge '1' ]] && [[ -f ${rwwc} ]]; the
   memuqinglong "$@"
 else
   memu "$@"
-echo
+fi
 
