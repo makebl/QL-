92f6f5bab81a0b1ecc094c27dd0fdf130a464423
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-24 15:57:16 +0800

    Update lang18.sh

--- lang18.sh
+++ lang18.sh
@@ -185,13 +185,11 @@ function qinglong_port() {
   esac
 }
 function system_check() {
-if [[ $synology == 1 ]]; then
-echo
-elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
+  if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
     ECHOG "正在安装宿主机所需要的依赖，请稍后..."
     export QL_PATH="/opt"
     yum -y install sudo wget git unzip net-tools.x86_64 subversion
-elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
     ECHOG "正在安装宿主机所需要的依赖，请稍后..."
     export QL_PATH="/opt"
     apt-get -y update
@@ -201,16 +199,35 @@ elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
     export QL_PATH="/opt"
     apt -y update
     apt -y install sudo wget git unzip net-tools subversion
-elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
-  export Aptget="opkg"
-  opkg update
-  opkg install git-http > /dev/null 2>&1
-  opkg install ca-bundle > /dev/null 2>&1
-  opkg install coreutils-timeout > /dev/null 2>&1
-  opkg install findutils-xargs > /dev/null 2>&1
-  opkg install unzip
-else
-  echo
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apk update
+    apk add sudo wget git unzip net-tools subversion
+  elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    opkg update
+    opkg install git-http > /dev/null 2>&1
+    opkg install ca-bundle > /dev/null 2>&1
+    opkg install coreutils-timeout > /dev/null 2>&1
+    opkg install findutils-xargs > /dev/null 2>&1
+    opkg install unzip
+    XTong="openwrt"
+    if [[ -d /opt/docker ]]; then
+      export QL_PATH="/opt"
+      export QL_Kongjian="/opt/docker"
+    elif [[ -d /mnt/mmcblk2p4/docker ]]; then
+      export QL_PATH="/root"
+      export QL_Kongjian="/mnt/mmcblk2p4/docker"
+    else
+      print_error "没找到/opt/docker或者/mnt/mmcblk2p4/docker"
+      exit 1
+    fi
+  else
+    print_error "不支持您的系统"
+    exit 1
+  fi
+}
 
 function kaiqiroot_ssh() {
   if [[ ! -f /etc/openwrt_release ]] && [[ ! -f /rom/etc/openwrt_release ]]; then
@@ -221,6 +238,8 @@ function kaiqiroot_ssh() {
     sleep 3
   fi
 }
+
+
 
 function nolanjdc_lj() {
   export Home="$QL_PATH/nolanjdc"
