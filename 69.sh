607b05407e6aa7e46ad2d2f8e8afee8c95ad2d36
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-25 22:25:53 +0800

    Update and rename 68.sh to 69.sh

--- 69.sh
+++ 69.sh
@@ -80,6 +80,124 @@ if [[ ! "$USER" == "root" ]]; then
   exit 1
 fi
 
+checkos(){
+  ifTermux=$(echo $PWD | grep termux)
+  ifMacOS=$(uname -a | grep Darwin)
+  if [ -n "$ifTermux" ];then
+    os_version=Termux
+  elif [ -n "$ifMacOS" ];then
+    os_version=MacOS  
+  else  
+    os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+  fi
+  
+  if [[ "$os_version" == "2004" ]] || [[ "$os_version" == "10" ]] || [[ "$os_version" == "11" ]];then
+    ssll="-k --ciphers DEFAULT@SECLEVEL=1"
+  fi
+}
+checkos 
+
+checkCPU(){
+  CPUArch=$(uname -m)
+  if [[ "$CPUArch" == "aarch64" ]];then
+    arch=linux_arm64
+  elif [[ "$CPUArch" == "i686" ]];then
+    arch=linux_386
+  elif [[ "$CPUArch" == "arm" ]];then
+    arch=linux_arm
+  elif [[ "$CPUArch" == "x86_64" ]] && [ -n "$ifMacOS" ];then
+    arch=darwin_amd64
+  elif [[ "$CPUArch" == "x86_64" ]];then
+    arch=linux_amd64    
+  fi
+}
+checkCPU
+check_dependencies(){
+
+  os_detail=$(cat /etc/os-release 2> /dev/null)
+  if_debian=$(echo $os_detail | grep 'ebian')
+  if_redhat=$(echo $os_detail | grep 'rhel')
+  if [ -n "$if_debian" ];then
+    InstallMethod="apt"
+  elif [ -n "$if_redhat" ] && [[ "$os_version" -lt 8 ]];then
+    InstallMethod="yum"
+  elif [[ "$os_version" == "MacOS" ]];then
+    InstallMethod="brew"  
+  fi
+}
+check_dependencies
+#安装wget、curl、unzip
+${InstallMethod} install unzip wget curl -y > /dev/null 2>&1 
+get_opsy() {
+  [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+  [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+  [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+virt_check() {
+  # if hash ifconfig 2>/dev/null; then
+  # eth=$(ifconfig)
+  # fi
+
+  virtualx=$(dmesg) 2>/dev/null
+
+  if [[ $(which dmidecode) ]]; then
+    sys_manu=$(dmidecode -s system-manufacturer) 2>/dev/null
+    sys_product=$(dmidecode -s system-product-name) 2>/dev/null
+    sys_ver=$(dmidecode -s system-version) 2>/dev/null
+  else
+    sys_manu=""
+    sys_product=""
+    sys_ver=""
+  fi
+
+  if grep docker /proc/1/cgroup -qa; then
+    virtual="Docker"
+  elif grep lxc /proc/1/cgroup -qa; then
+    virtual="Lxc"
+  elif grep -qa container=lxc /proc/1/environ; then
+    virtual="Lxc"
+  elif [[ -f /proc/user_beancounters ]]; then
+    virtual="OpenVZ"
+  elif [[ "$virtualx" == *kvm-clock* ]]; then
+    virtual="KVM"
+  elif [[ "$cname" == *KVM* ]]; then
+    virtual="KVM"
+  elif [[ "$cname" == *QEMU* ]]; then
+    virtual="KVM"
+  elif [[ "$virtualx" == *"VMware Virtual Platform"* ]]; then
+    virtual="VMware"
+  elif [[ "$virtualx" == *"Parallels Software International"* ]]; then
+    virtual="Parallels"
+  elif [[ "$virtualx" == *VirtualBox* ]]; then
+    virtual="VirtualBox"
+  elif [[ -e /proc/xen ]]; then
+    virtual="Xen"
+  elif [[ "$sys_manu" == *"Microsoft Corporation"* ]]; then
+    if [[ "$sys_product" == *"Virtual Machine"* ]]; then
+      if [[ "$sys_ver" == *"7.0"* || "$sys_ver" == *"Hyper-V" ]]; then
+        virtual="Hyper-V"
+      else
+        virtual="Microsoft Virtual Machine"
+      fi
+    fi
+  else
+    virtual="Dedicated母鸡"
+  fi
+}
+get_system_info() {
+  cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')
+  opsy=$(get_opsy)
+  arch=$(uname -m)
+
+  kern=$(uname -r)
+
+  virt_check
+}
+copyright(){
+    clear
+echo -e "
+
+
 export Current="$PWD"
 
 function qinglong_port() {
