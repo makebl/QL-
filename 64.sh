13b8d58912937bb805442c0f498a1a166d582f51
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-25 22:00:46 +0800

    Update and rename 63.sh to 64.sh

--- 64.sh
+++ 64.sh
@@ -181,36 +181,52 @@ function qinglong_port() {
 }
 
 function system_check() {
-if [[ $synology == 1 ]]; then
-  echo
-  ECHOG "群晖nas"
-  echo
-elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
-  export Aptget="yum"
-  yum -y update
-  yum install -y sudo wget curl psmisc net-tools
-  export XITONG="cent_os"
-elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
-  export Aptget="apt-get"
-  apt-get -y update
-  apt-get install -y sudo wget curl psmisc net-tools
-  export XITONG="ubuntu_os"
-elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" || "$(. /etc/os-release && echo "$ID")" == "Deepin" ]]; then
-  export Aptget="apt"
-  apt-get -y update
-  apt-get install -y sudo wget curl psmisc net-tools
-  export XITONG="debian_os"
-elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
-  export Aptget="opkg"
-  opkg update
-  opkg install git-http > /dev/null 2>&1
-  opkg install ca-bundle > /dev/null 2>&1
-  opkg install coreutils-timeout > /dev/null 2>&1
-  opkg install findutils-xargs > /dev/null 2>&1
-  opkg install unzip
-fi
+  if [[ $synology == 1 ]]; then
+  export QL_PATH="/opt"
+  ipkg update
+  ipkg install sudo wget git unzip net-tools subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    yum -y install sudo wget git unzip net-tools.x86_64 subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "ubuntu" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apt-get -y update
+    apt-get -y install sudo wget git unzip net-tools subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "debian" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apt -y update
+    apt -y install sudo wget git unzip net-tools subversion
+  elif [[ "$(. /etc/os-release && echo "$ID")" == "alpine" ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    export QL_PATH="/opt"
+    apk update
+    apk add sudo wget git unzip net-tools subversion
+  elif [[ -f /etc/openwrt_release ]] && [[ -f /rom/etc/openwrt_release ]]; then
+    ECHOG "正在安装宿主机所需要的依赖，请稍后..."
+    opkg update
+    opkg install git-http > /dev/null 2>&1
+    opkg install ca-bundle > /dev/null 2>&1
+    opkg install coreutils-timeout > /dev/null 2>&1
+    opkg install findutils-xargs > /dev/null 2>&1
+    opkg install unzip
+    XTong="openwrt"
+    if [[ -d /opt/docker ]]; then
+      export QL_PATH="/opt"
+      export QL_Kongjian="/opt/docker"
+    elif [[ -d /mnt/mmcblk2p4/docker ]]; then
+      export QL_PATH="/root"
+      export QL_Kongjian="/mnt/mmcblk2p4/docker"
+    else
+      print_error "没找到/opt/docker或者/mnt/mmcblk2p4/docker"
+      exit 1
+    fi
+  else
+    print_error "不支持您的系统"
     exit 1
-
+  fi
 }
 
 function kaiqiroot_ssh() {
