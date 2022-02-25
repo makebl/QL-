96c99a2339f9b65b711fb8963063d35d27a68f4d
279437541 <78850005+279437541@users.noreply.github.com>
2022-02-25 20:53:41 +0800

    Update and rename 25.sh to 26.sh

--- 26.sh
+++ 26.sh
@@ -175,6 +175,60 @@ function qinglong_port() {
   esac
 }
 
+checkos(){
+  ifTermux=$(echo $PWD | grep termux)
+  ifMacOS=$(uname -a | grep Darwin)
+  if [ -n "$ifTermux" ];then
+    os_version=Termux
+  elif [ -n "$ifMacOS" ];then
+    os_version=MacOS  
+  else  
+    os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+  fi
+  
+  if [[ "$os_version" == "2004" ]] || [[ "$os_version" == "10" ]] || [[ "$os_version" == "11" ]];then
+    ssll="-k --ciphers DEFAULT@SECLEVEL=1"
+  fi
+}
+checkos 
+
+checkCPU(){
+  CPUArch=$(uname -m)
+  if [[ "$CPUArch" == "aarch64" ]];then
+    arch=linux_arm64
+  elif [[ "$CPUArch" == "i686" ]];then
+    arch=linux_386
+  elif [[ "$CPUArch" == "arm" ]];then
+    arch=linux_arm
+  elif [[ "$CPUArch" == "x86_64" ]] && [ -n "$ifMacOS" ];then
+    arch=darwin_amd64
+  elif [[ "$CPUArch" == "x86_64" ]];then
+    arch=linux_amd64    
+  fi
+}
+checkCPU
+check_dependencies(){
+
+  os_detail=$(cat /etc/os-release 2> /dev/null)
+  if_debian=$(echo $os_detail | grep 'ebian')
+  if_redhat=$(echo $os_detail | grep 'rhel')
+  if [ -n "$if_debian" ];then
+    InstallMethod="apt"
+  elif [ -n "$if_redhat" ] && [[ "$os_version" -lt 8 ]];then
+    InstallMethod="yum"
+  elif [[ "$os_version" == "MacOS" ]];then
+    InstallMethod="brew"  
+  fi
+}
+check_dependencies
+#安装wget、curl、unzip
+${InstallMethod} install unzip wget curl -y > /dev/null 2>&1 
+get_opsy() {
+  [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+  [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+  [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
 function system_check() {
 if [[ "$(. /etc/os-release && echo "$ID")" == "centos" ]]; then
    yum install git -y > /dev/null
